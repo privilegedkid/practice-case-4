b81d092b21c1f799ab13e906e0ab8d9efc0502ca
privilegedkid <kalushin03@bk.ru>
2026-08-14 16:57:34 +0500

    Initialize Project1 with WebBroker and ISAPIApp

---
+++ Project1.dpr
@@ -0,0 +1,19 @@
+library Project1;
+
+uses
+  Web.WebBroker,
+  Web.Win.ISAPIApp,
+  WebModuleUnit1 in 'WebModuleUnit1.pas' {WebModule1: TWebModule};
+
+{$R *.res}
+
+exports
+  GetExtensionVersion,
+  HttpExtensionProc,
+  TerminateExtension;
+
+begin
+  Application.Initialize;
+  Application.WebModuleClass := WebModuleClass;
+  Application.Run;
+end.
